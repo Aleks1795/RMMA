da26dd336446974b83ed4cd9efd44921478af15b
Aleks1795 <aleks1795@icloud.com>
2022-12-05 13:19:48 +0300

    Initial commit

---
+++ app/src/main/java/com/ruslanchikbananchik/rmma/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ruslanchikbananchik.rmma
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ruslanchikbananchik.rmma.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.helloAndroid.text = getString(R.string.hello_android, Build.VERSION.RELEASE)
+    }
+
+
+
+
+
+}
